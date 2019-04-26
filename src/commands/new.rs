713c73b51b3e3b089fb6692de19917e30359517d
Adilson Schmitt Junior <adilsonschj@gmail.com>
2019-04-26 17:05:29 +0200

    experiments

--- src/commands/new.rs
+++ src/commands/new.rs
@@ -1,5 +1,51 @@
+use clap::{App, Arg, SubCommand, ArgMatches};
 use std::fs;
+use std::default::Default;
 
-pub fn execute_command(template: &str, output: &str) {
-    fs::write(output, template).expect(format!("Unable to create file {}", output));
+pub static NAME: &'static str = "new";
+pub static ARGS_TEMPLATE: &'static str = "template";
+pub static ARGS_OUTPUTFILE: &'static str = "OUTPUT_FILE";
+
+arg_enum!{
+    enum Templates {
+        GET,
+        POST,
+        BLANK
+    }
+}
+
+impl Default for Templates {
+    fn default() -> Templates { Templates::GET } 
+}
+
+
+pub fn get_definition<'a, 'b>() -> App<'a, 'b> {
+    SubCommand::with_name("new")
+        .about("create a new file that describes a request")
+        .arg(
+            Arg::with_name(ARGS_TEMPLATE)
+                .help("Template for the file")
+                .short("t")
+                .long("template")
+        )
+        .arg(
+            Arg::with_name(ARGS_OUTPUTFILE)
+                .help("Where to create the file")
+                .required(true),
+        )
+}
+
+pub fn execute_command<'a>(matches: &ArgMatches<'a>) {
+    let template = value_t!(matches.value_of(ARGS_TEMPLATE), Templates).unwrap_or_else(|e| e.exit());
+    let output = matches.value_of(ARGS_OUTPUTFILE).unwrap();
+
+    
+
+    let data = match template {
+        Templates::GET => "{\"path\":\"http://localhost/your/path/here\", \"method\":\"GET\" }",
+        Templates::POST => "{\"path\":\"http://localhost/your/path/here\", \"method\":\"POST\", }",
+        _ => ""
+    };
+
+    fs::write(output, data).expect(&format!("Unable to create file {}", output));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,18 @@
+#[macro_use]
 extern crate clap;
-use clap::{Arg, App, SubCommand};
+use clap::{App};
+mod commands;
 
 fn main() {
     let matches = App::new("WePost automating your requests")
         .version("0.0.1")
         .author("Adilson Schmitt Junior")
-        .subcommand(SubCommand::with_name("new")
-            .about("create a new file that describes a request")
-            .arg(Arg::with_name("OUTPUT_FILE")
-                .help("Where to create the file")
-                .required(true)
-                .last(true)
-            )
-            .arg(Arg::with_name("template")
-                .help("Template for the file")
-                .short("t")
-                .long("template")
-                .default_value("GET")
-            )
-            
+        .subcommand(
+            commands::new::get_definition()
         )
         .get_matches();
+    
+    if let Some(new_matches) = matches.subcommand_matches(commands::new::NAME) {
+        commands::new::execute_command(new_matches);
+    }
 }
